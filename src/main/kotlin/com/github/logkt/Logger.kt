15401fed09f48e454deb1f9fb63776d9a20f7129
Tem3dy <tem3dy@gmail.com>
2024-11-18 02:11:56 +0100

    refactor: implement logging queue

--- src/main/kotlin/com/github/logkt/Logger.kt
+++ src/main/kotlin/com/github/logkt/Logger.kt
@@ -1,28 +1,37 @@
 package com.github.logkt
 
 import kotlinx.coroutines.*
+import java.util.concurrent.BlockingQueue
+import java.util.concurrent.LinkedBlockingQueue
 
 class Logger(val name: String, var level: Level = Level.ERROR) {
     private var job: Job? = null
     private val scope = CoroutineScope(Dispatchers.Default)
 
-    fun open() {
-        job = scope.launch { run() }
-    }
+    private val records: BlockingQueue<LogRecord> = LinkedBlockingQueue()
+    private val out = System.out.bufferedWriter(Charsets.UTF_8)
+
+    fun open() { job = scope.launch { run() } }
 
     private fun run(): Job {
         return scope.launch {
             while (isActive) {
                 try {
-                    // logger logic
-                } catch (error: Error) {
+                    val record = records.take()
+                    if (level == Level.OFF) return@launch
+
+                    if (level.ordinal >= record.level.ordinal) {
+                        // write into out
+                    }
+                } catch (error: Exception) {
                     // handle errors
+                    close()
                 }
             }
         }
     }
 
     fun close() {
-        job?.cancel() // Cancel the coroutine
+        job?.cancel()
     }
 }
