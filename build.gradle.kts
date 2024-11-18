6cca45fca4e2321f56870344e2748fadd88c5021
Tem3dy <tem3dy@gmail.com>
2024-11-18 01:57:09 +0100

    build: added coroutines dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,9 @@ plugins {
     kotlin("jvm") version "2.0.21"
 }
 
+repositories { mavenCentral() }
+dependencies { implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1") }
+
 java {
     toolchain { languageVersion = JavaLanguageVersion.of(17) }
 }
